245713f175df576572a6cda49f472369828e4bf5
Trench <31423299+twocatsinatrenchcoat@users.noreply.github.com>
2022-12-06 10:17:44 +0100

    Create day6.py

---
+++ day6.py
@@ -0,0 +1,17 @@
+inpt = open("input.txt","r").read().strip()
+
+def findNUnique(n,string):
+    marker = 0
+    while marker < len(string):
+        valid = True
+        attempt = string[marker:marker+n]
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if attempt[i]==attempt[j]:
+                    valid = False
+                    marker += max(i,1)
+        if valid:
+            return marker
+
+print("Part 1:",findNUnique(4,inpt)+4)
+print("Part 2:",findNUnique(14,inpt)+14)
